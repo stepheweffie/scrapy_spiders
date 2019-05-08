b6b70172965550d43332995a649cf074ad0aadbf
wtata <wtata@protonmail.com>
2019-05-07 21:09:34 -0400

    Update requirements.txt, add spider variables

--- quotes/quotes/spiders/quotes_spider.py
+++ quotes/quotes/spiders/quotes_spider.py
@@ -8,4 +8,6 @@ class QuotesSpiderSpider(scrapy.Spider):
     start_urls = ['http://http://quotes.toscrape.com//']
 
     def parse(self, response):
-        pass
+        h1_tag = response.xpath('//h1/a/text()').extract_first()
+        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        yield {'H1 Tag': h1_tag, 'Tags': tags}
